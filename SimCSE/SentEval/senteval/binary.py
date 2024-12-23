cd89cd8a393a5bd65f60333f759cf4c3f3020a05
Siladittya Manna <sadimanna@users.noreply.github.com>
2024-12-23 21:12:36 +0800

    Update binary.py

--- SimCSE/SentEval/senteval/binary.py
+++ SimCSE/SentEval/senteval/binary.py
@@ -3,7 +3,7 @@
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-#
+
 
 '''
 Binary classifier and corresponding datasets : MR, CR, SUBJ, MPQA
