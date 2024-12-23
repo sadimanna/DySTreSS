887daa02e516de9c47050e8de5a0c9c44cb47231
Siladittya Manna <sadimanna@users.noreply.github.com>
2024-12-23 20:50:23 +0800

    IEEE TAI 2024

--- SimCSE/SentEval/examples/skipthought.py
+++ SimCSE/SentEval/examples/skipthought.py
@@ -3,7 +3,7 @@
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-#
+
 
 from __future__ import absolute_import, division, unicode_literals
 
