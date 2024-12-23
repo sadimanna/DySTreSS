ce2f6c44b35c7d07b999745d943ad922f22d2615
Siladittya Manna <sadimanna@users.noreply.github.com>
2024-12-23 20:48:54 +0800

    IEEE TAI 2024

--- SimCSE/SentEval/examples/models.py
+++ SimCSE/SentEval/examples/models.py
@@ -3,7 +3,7 @@
 #
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-#
+
 
 """
 This file contains the definition of encoders used in https://arxiv.org/pdf/1705.02364.pdf
